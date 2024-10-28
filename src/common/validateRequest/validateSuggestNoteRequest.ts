efc23dab7f5f42c44a63947c8b9736ad93e057fd
ThangNgo <ngosythang123456@gmail.com>
2024-10-29 02:45:32 +0700

    feat: update validate empty trim suggest note

--- src/common/validateRequest/validateSuggestNoteRequest.ts
+++ src/common/validateRequest/validateSuggestNoteRequest.ts
@@ -6,15 +6,24 @@ export function validateSuggestNoteRequest(req: Request) {
     throw new Error("selectedNode is required and must be an object.");
   }
 
-  if (!selectedNode.id || !selectedNode.name) {
-    throw new Error("selectedNode.id and selectedNode.name are required.");
-  }
-  if (typeof selectedNode.id !== "string" || !selectedNode.id) {
-    throw new Error("selectedNode.id is required and must be a string.");
+  if (
+    typeof selectedNode.id !== "string" ||
+    !selectedNode.id ||
+    selectedNode.id.trim() === ""
+  ) {
+    throw new Error(
+      "selectedNode.id is required and must be a string and not empty."
+    );
   }
 
-  if (typeof selectedNode.name !== "string" || !selectedNode.name) {
-    throw new Error("selectedNode.name is required and must be a string.");
+  if (
+    typeof selectedNode.name !== "string" ||
+    !selectedNode.name ||
+    selectedNode.name.trim() === ""
+  ) {
+    throw new Error(
+      "selectedNode.name is required and must be a string and not empty."
+    );
   }
 
   return {
